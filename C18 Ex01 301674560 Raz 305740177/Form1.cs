a0052845dc0f3c96024e2a28059d3fc3d93464fe
Erez MItelka <mitelka2013@gmail.com>
2018-07-20 13:20:34 +0300

    Add project files.

---
+++ C18 Ex01 301674560 Raz 305740177/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C18_Ex01_301674560_Raz_305740177
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
