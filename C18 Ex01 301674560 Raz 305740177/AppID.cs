2b0850cb070607bb86060e150f01a1c223a76625
Erez MItelka <mitelka2013@gmail.com>
2018-07-21 15:58:36 +0300

    first login

---
+++ C18 Ex01 301674560 Raz 305740177/AppID.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C18_Ex01_301674560_Raz_305740177
+{
+    public class AppId
+    {
+        public string Value { get; set; }
+
+        public AppId(string i_Value)
+        {
+            Value = i_Value;
+        }
+    }
+}
